73729b470fbd5605d2ef51a495eabcaf371b3a5c
TheoTr <71769486+TheoTr@users.noreply.github.com>
2023-05-15 14:53:28 +0200

    Rename variable execpt to expect

--- py_wip/tests/test_analyzer.py
+++ py_wip/tests/test_analyzer.py
@@ -8,20 +8,20 @@ class TestAnalyzer(unittest.TestCase):
     def test_analyzer(self):
         # Arrange
         listanalyzer = "10/100,t1+20/100,t2+30/100,t3"
-        exceptresult = [('10', '100', 't1'), ('20', '100', 't2'), ('30', '100', 't3')]
+        expectresult = [('10', '100', 't1'), ('20', '100', 't2'), ('30', '100', 't3')]
         # Act
         analyzer = ListAnalyzer(listanalyzer)
         result = analyzer.golist()
         # Assert
-        self.assertEqual(check_validity(result, exceptresult), True)
+        self.assertEqual(check_validity(result, expectresult), True)
 
 
     def test_formula(self):
         # Arrange
         formula = "50%t1+20%t2+30%t3"
-        exceptresult = [(50.0, 't1'), (20.0, 't2'), (30.0, 't3')]
+        expectresult = [(50.0, 't1'), (20.0, 't2'), (30.0, 't3')]
         # Act
         parser = FormulaParser(formula)
         result = parser.decompose()
         # Assert
-        self.assertEqual(check_validity(result, exceptresult), True)
+        self.assertEqual(check_validity(result, expectresult), True)
