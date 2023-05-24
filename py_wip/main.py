5a3099ab081282c4bc9fc956b3c6ab4fd0c2343b
Paul Maris <paul.marisoumary@algosup.com>
2023-05-24 12:16:58 +0200

    comment + remove useless tanks

--- py_wip/main.py
+++ py_wip/main.py
@@ -1,28 +1,38 @@
 import random
 
 from src.tank import Tank
-from src.utils import create_nodes, print_tanks_nodes, theoretical_max
+from src.utils import create_nodes, print_tanks_nodes, remove_useless_tanks, theoretical_max
 
 N_TANKS = 10
 
 MINL_TANK = 100
 MAXL_TANK = 300
 
+FORMULA = "25%t1+50%t2+12.5%t3+12.5%t4"
+
 if __name__ == "__main__":
     random.seed(0)
 
     random.randint(MINL_TANK, MAXL_TANK)
 
+    # Create tanks
     tanks: list[Tank] = []
     for i in range(1, N_TANKS+1):
         ml = random.randint(MINL_TANK, MAXL_TANK)
         #tanks.append(Tank('t'+str(i), ml, level=0 if i % 2 == 0 else ml)) # 0 or | FULL
         tanks.append(Tank('t'+str(i), ml, level=random.randint(0, ml))) # 0 to -> FULL
 
-    create_nodes(tanks)
-    print_tanks_nodes(tanks)
+    # Get theoretical max based on formula
+    max_blend = theoretical_max(tanks, FORMULA)
 
-    print(theoretical_max(tanks, "25%t1+50%t2+12.5%t3+12.5%t4"))
+    # Remove useless tanks in our list
+    remove_useless_tanks(tanks, FORMULA)
+
+    # Create nodes for each tank
+    create_nodes(tanks)
+
+    #? Print tanks and their nodes (DEBUG)
+    print_tanks_nodes(tanks)
 
 def wip_test():
     # Check different possible mixes between tanks
